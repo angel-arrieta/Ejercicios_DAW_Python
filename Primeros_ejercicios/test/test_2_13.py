b449e462e39c78018bfeb07343109a20ab147b78
Ejercicios_DAW_Python <a.arrieta03soto@gmail-com>
2023-10-07 19:42:11 +0200

    Creación Ej Y Test 14

---
+++ Primeros_ejercicios/test/test_2_13.py
@@ -0,0 +1,5 @@
+from Ejercicios_DAW_Python.Primeros_ejercicios.src.Ej_2_13 import div_por_partes
+
+
+def test_div_por_partes():
+    assert div_por_partes(7,3) == f"la división de 7 entre 3 da un cociente 2 y un resto 1"
